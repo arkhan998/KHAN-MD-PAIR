f83a9ba756630802213f98d1c266bea71ca7da3b
Ata Ur Rehman Malik <119836227+arkhan998@users.noreply.github.com>
2024-07-19 10:07:37 +0500

    Update DJMODZX.js

--- DJMODZX.js
+++ DJMODZX.js
@@ -1,26 +1,29 @@
+```
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const path = require('path');
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
+const server = require('./qr');
+const code = require('./pair');
+
 require('events').EventEmitter.defaultMaxListeners = 500;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/qr', server);
 app.use('/code', code);
 app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
+    res.sendFile(path.join(__dirname, '/pair.html'))
 })
 app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
+    res.sendFile(path.join(__dirname, '/main.html'))
 })
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
-    console.log(`
-Powered by DJMODZX
-
- Server running on http://localhost:` + PORT)
+    console.log(`Powered by DJMODZX Server running on http://localhost:${PORT}`)
 })
 
-module.exports = app
+module.exports = app;
+```
